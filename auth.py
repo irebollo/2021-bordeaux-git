b9022ba596b2a7c1f085cbb05ce3874b49095b2f
Tiziano Zito <opossumnano@gmail.com>
2021-08-23 10:20:41 +0200

    implemented authenticate

--- auth.py
+++ auth.py
@@ -3,5 +3,19 @@ def get_credentials():
     password = input('Type the password: ')
     return username, password
 
+def authenticate(user, password, pwdb):
+    if user in pwdb:
+        if password == pwdb[user]:
+            print('Successfully authenticated!')
+        else:
+            print('Wrong password!!')
+    else:
+        add = input('Add user to the db? ')
+        if add == 'y':
+            pwdb[user] = password
+        else:
+            pass
+    return
+
 print(get_credentials())
 
